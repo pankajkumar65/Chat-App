61f03f566f91a23c88895f059180228bf8355fb7
pankaj <pankajkr97397@gmail.com>
2023-08-19 12:59:51 +0530

    third commit

--- js/client.js
+++ js/client.js
@@ -1,11 +1,15 @@
 const socket = io('http://localhost:8000');
 
+// get Dom elements in receving js variable
 const form = document.getElementById("send-container");
 const messageinput = document.getElementById("messageInp");
 const messageContainer = document.querySelector(".container");
+
+//Audio that will play on receving message
 const tingMsg =  new Audio("../ting.mp3");
 const tick = new Audio("../tick.mp3");
 
+//function which will append to the container
 const append = (message,position)=>{
     const messageElement = document.createElement("div");
     messageElement.innerText = message;
@@ -13,28 +17,33 @@ const append = (message,position)=>{
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
 }
-form.addEventListener("submit",(e)=>{
-     e.preventDefault();
-     const message = messageinput.value;
-     append(`you : ${message}`,"right");
-     socket.emit('send',message);
-     messageinput.value = '';
-     tick.play();  
-})
+
+//Ask new user for his name and let the server know
 const name = prompt("Enter your name to join");
 socket.emit("new-user-joined",name);
-append(`welcome to our chat ${name}!`,'mid')
 
-
+// if new user joins, receive  the event from the server
 socket.on('user-joined',name=>{
-    append(`${name} : joined the chat...`,'right');
+    append(`${name} : joined the chat`,'right');
 })
+
+//if server sends the message, receive it
 socket.on('receive',data=>{
     append(`${data.name} : ${data.message}`,'left');
-    // tingMsg.play();
+    tingMsg.play();
 })
 
+// if useer leaves the chat, append the info into to the container
 socket.on('left',name=>{
-    append(`${name} : left the chat!`,'left');
+    append(`${name} : left the chat!`,'mid');
 })
  
+// if the form get submitted send server the message
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const message = messageinput.value;
+    append(`you : ${message}`,"right");
+    socket.emit('send',message);
+    messageinput.value = '';
+   //  tick.play();  
+})
--- nodeserver/index.js
+++ nodeserver/index.js
@@ -5,17 +5,19 @@ const io = require('socket.io')(8000,{
     },
 });
 const users = {};
-
+// if new user joined let other people know.
 io.on('connection',socket=>{
     socket.on('new-user-joined', name=>{
         users[socket.id] = name;
         socket.broadcast.emit('user-joined',name);
     });
 
+    // if someone sends the messsge brosdcast to the other ppeople.
     socket.on('send',message=>{
         socket.broadcast.emit('receive',{message: message, name: users[socket.id]})
     });
 
+    // if someone leaves the chat let other people know.
     socket.on('disconnect',message=>{
         socket.broadcast.emit('left',users[socket.id]);
         delete users[socket.id];
